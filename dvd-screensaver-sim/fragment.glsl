a211bbcb19d3c00ed103cc690f9e21aa68d90eb9
sldr <sldr@laptop>
2024-08-29 15:32:13 +0300

    V1.1 - fixed bugs and added corner hit effect

--- dvd-screensaver-sim/fragment.glsl
+++ dvd-screensaver-sim/fragment.glsl
@@ -3,11 +3,12 @@
 out vec4 frag_color;
 
 in vec2 tex_coords;
+in vec3 vertex_color;
 uniform vec3 u_color;
 
 uniform sampler2D texture1;
 
 void main()
 {
-    frag_color = texture(texture1, tex_coords) * vec4(u_color, 1.0);
+    frag_color = (u_color != vec3(0.0, 0.0, 0.0)) ? (texture(texture1, tex_coords) * vec4(u_color, 1.0)) : (texture(texture1, tex_coords) * (vec4(vertex_color, 1.0)));
 }
--- dvd-screensaver-sim/vertex.glsl
+++ dvd-screensaver-sim/vertex.glsl
@@ -1,12 +1,15 @@
 #version 330 core
 layout (location = 0) in vec3 a_pos;
-layout (location = 1) in vec2 a_tex_coords;
+layout (location = 1) in vec3 a_color;
+layout (location = 2) in vec2 a_tex_coords;
 
 uniform vec2 velocity;
 out vec2 tex_coords;
+out vec3 vertex_color;
 
 void main()
 {
     gl_Position = vec4(a_pos.x + velocity.x, a_pos.y + velocity.y, a_pos.z, 1.0);
     tex_coords = a_tex_coords;
+    vertex_color = a_color;
 }
